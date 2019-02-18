55e09cc697f0c329b0cf965c41b004be81caa221
Zaineb Lamine <zaineb.lamine@vistaprint.com>
2019-02-18 15:21:58 +0100

    DG-25737: adding prometheus

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,19 +2,20 @@ package hello;
 
 import java.util.Arrays;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({com.vpdigital.MetricsComponents.class})
 public class Application {
-    
+
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        
+
         System.out.println("Let's inspect the beans provided by Spring Boot:");
         
-        String[] beanNames = ctx.getBeanDefinitionNames();
+        String[] beanNames = new SpringApplicationBuilder(Application.class)
+                .run(args).getBeanDefinitionNames();
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
             System.out.println(beanName);
